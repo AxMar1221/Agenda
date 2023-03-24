602dd41a864185bdf102c3893a998d092721bdde
Tachi Dito <tachidito_7inc@hotmail.co>
2023-03-23 19:34:42 -0600

    SweetAlert updateNote

--- src/agenda/view/NoteView.jsx
+++ src/agenda/view/NoteView.jsx
@@ -3,15 +3,16 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { SaveOutlined } from "@mui/icons-material";
 import { Button, Grid, TextField, Typography } from "@mui/material";
+import Swal from "sweetalert2";
+import 'sweetalert2/dist/sweetalert2.css'
 
 import { useForm } from "../../hooks/useForm";
 import { ImageGallery } from "../components";
-import { setActiveNote } from "../../store/agenda/agendaSlice";
-import { startSaveNote } from "../../store/agenda/thunks";
+import { setActiveNote, startSaveNote } from "../../store/agenda";
 
 export const NoteView = () => {
   const dispatch = useDispatch();
-  const { active: note } = useSelector((state) => state.agenda);
+  const { active: note, messageSaved, isSaving } = useSelector((state) => state.agenda);
   const { body, title, date, onInputChange, formState } = useForm(note);
 
   const dateString = useMemo(() => {
@@ -22,9 +23,15 @@ export const NoteView = () => {
     dispatch(setActiveNote(formState));
   }, [formState]);
 
+  useEffect(() => {
+    if ( messageSaved.length > 0 ) {
+      Swal.fire("Nota actualizada", messageSaved, "success");
+    }
+  }, [messageSaved]);
+
   const onSaveNote = () => {
-    dispatch( startSaveNote() );
-  }
+    dispatch(startSaveNote());
+  };
 
   return (
     <Grid
@@ -44,7 +51,8 @@ export const NoteView = () => {
         <Button
           color="primary"
           sx={{ padding: 2 }}
-          onClick={ onSaveNote }
+          onClick={onSaveNote}
+          disabled={ isSaving }
         >
           <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
           Guardar
